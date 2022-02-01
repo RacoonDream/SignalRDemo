a645a48dd82af2661c7d81d649a2bafa76e575fc
RacoonDream <chikucoder@gmail.com>
2022-02-01 16:25:28 +0530

    first commit

---
+++ WiredBrain/Hubs/CoffeeHub.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using WiredBrain.Helpers;
+
+namespace WiredBrain.Hubs
+{
+    public class CoffeeHub : Hub
+    {
+        private readonly OrderChecker _orderChecker;
+
+        public CoffeeHub(OrderChecker orderChecker)
+        {
+            _orderChecker = orderChecker;
+        }
+
+        public async Task GetUpdateForOrder(int orderId)
+        {
+            CheckResult result;
+            do
+            {
+                result = _orderChecker.GetUpdate(orderId);
+                Thread.Sleep(1000);
+                if (result.New)
+                    await Clients.Caller.SendAsync("ReceiveOrderUpdate", result.Update);
+            } while (!result.Finished);
+            await Clients.Caller.SendAsync("Finished");
+        }
+        public override Task OnConnectedAsync()
+        {
+            //var connectionId = Context.ConnectionId;
+            //Context.User.Claims
+            //Clients.Group("").SendAsync().ConfigureAwait.
+            return base.OnConnectedAsync();
+        }
+    }
+}
